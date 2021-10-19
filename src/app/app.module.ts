5d84195455c6ea763b05eadee6e40baa7fe2692b
Jaxmie <unclejacktalent@gmail.com>
2021-10-19 20:00:23 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    ButtonComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
